1c9a2ebf6a4b8b4e8b011a13cf9023db284d6a6b
mi3er <mi3er@bk.ru>
2023-06-27 21:41:42 +0300

    Initial commit

---
+++ m16_HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m16_HW
+//
+//  Created by Павел Федотов on 27.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
